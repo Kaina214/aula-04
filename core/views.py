053e27d126610ffbf9ec69cdd70c829ff9941d91
KAINABRUMHABEKOST <kainabrum123@gmail.com>
2026-01-30 21:58:58 -0300

    novo chamdo

--- core/views.py
+++ core/views.py
@@ -9,11 +9,41 @@ chamados = [
     {"lab": "Lab 01", "problema": "PC lento", "prioridade": "Média"},
 ]
 
-def listar(request):
-    # Lógica para listar os chamados em HTML
-    return render(request, 'listar.html', {'chamados': chamados})
+def home(request):
 
-def criar(request, lab, problema, prioridade):
+    # Lógica para listar os chamados em HTML
+    return render(request, 'core/home.html', )
+
+    def novo_chamado(request):
+        return render(request, 'core/novo_chamado.html')
+        if (request.method == 'POST'):
+            lab = request.POST.get('laboratorio')
+            problema = request.POST.get('problema')
+            print("chegou um post")
+            print(f"Laboratório: {lab},Descrição: {problema}")
+            
+            chamados.append({
+                "lab": lab,
+                "problema": problema,
+                "prioridade": "Média"  # Prioridade fixa por enquanto
+            })
+            return redirect('/listar')
+
+            if request.method == 'GET':
+                print
+                return render(request, 'core/novo_chamado.html')
+
+
+
+
+
+
+
+
+    # return HttpResponse(render(request, 'listar.html', {'chamados': chamados}))
+    # def home_old(request):
+        # return HttpResponse("Bem-vindo ao sistema de chamados!")
+# def criar(request, lab, problema, prioridade):
     # Criando o dicionário e adicionando à lista
     novo = {
         "lab": lab,
@@ -34,3 +64,5 @@ def fechar(request, indice):
         'mensagem': 'Chamado removido com sucesso!',
         'tipo_acao': 'removido'
     })
+def listar(request):
+    return render(request, 'core/listar.html', {'chamados': chamados})
